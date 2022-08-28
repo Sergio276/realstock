b3f18c2449f4456e26e64c42a64f9cacdceb99cc
Sergio <checho140140@gmail.com>
2022-08-28 14:04:43 -0500

    validacion de contraseña

--- frm_registrar-usuarios.php
+++ frm_registrar-usuarios.php
@@ -86,11 +86,11 @@
                 </div>
                 <div class="form-section">
                   <label for="">Contraseña:</label>
-                  <input type="password" name="password" required placeholder="Ingrese la contraseña..." />
+                  <input type="password" minlength="8" name="password" required placeholder="Ingrese la contraseña..." />
                 </div>
                 <div class="form-section">
                   <label for="">Confirmar contraseña:</label>
-                  <input type="password" name="confirm_pass" required placeholder="Confirme la contraseña..." />
+                  <input type="password" minlength="8" name="confirm_pass" required placeholder="Confirme la contraseña..." />
                 </div>
                 <div class="form-section">
                   <label for="">Email:</label>
@@ -181,11 +181,11 @@
                           </div>
                           <div class="form-section">
                             <label for="">Contraseña:</label>
-                            <input type="password" placeholder="Ingrese la contraseña..." name="password" value="" />
+                            <input type="password" minlength="8" placeholder="Ingrese la contraseña..." name="password" value="" />
                           </div>
                           <div class="form-section">
                             <label for="">Confirmar contraseña:</label>
-                            <input type="password" placeholder="Confirme la contraseña..." name="confirm_pass" value="" />
+                            <input type="password" minlength="8" placeholder="Confirme la contraseña..." name="confirm_pass" value="" />
                           </div>
                           <div class="form-section">
                             <label for="">Email:</label>
--- php/update-user.php
+++ php/update-user.php
@@ -20,28 +20,51 @@ if (isset($_POST['update'], $_GET['user_id'])) {
 
     $user_id = $_GET['user_id'];
 
-    if ($password !== "") {
-        $password = password_hash($password, PASSWORD_DEFAULT, array('cost' => 12));
+    $stmt_verify = $conn->prepare("SELECT usuario FROM tbl_usuario WHERE usuario = ?");
+    $stmt_verify->bind_param("s", $user);
 
-        $stmt = $conn->prepare("UPDATE tbl_usuario SET usuario = ?, contrasena = ?, estado = ?, email = ?, nombres = ?, apellidos = ? WHERE usuario_id = '$user_id'");
-        $stmt->bind_param('ssssss', $user, $password, $status, $email, $name, $last_name);
+    $stmt_verify->execute();
+    $stmt_verify->store_result();
+
+    if (($stmt_verify) && ($stmt_verify->num_rows == 0)) {
+
+        if ($password !== "") {
+            $uppercase = preg_match('@[A-Z]@', $password);
+            $lowercase = preg_match('@[a-z]@', $password);
+            $number    = preg_match('@[0-9]@', $password);
+
+            if (!$uppercase || !$lowercase || !$number || strlen($password) < 8) {
+                echo '<script type="text/javascript">
+                alert("La contraseña no cumple con alguno de los requisitos de tener al menos una letra mayúscula, minúscula y un número.");
+                window.location.href = "../frm_registrar-usuarios.php";
+                </script>';
+            } else {
+                $password = password_hash($password, PASSWORD_DEFAULT, array('cost' => 12));
+
+                $stmt = $conn->prepare("UPDATE tbl_usuario SET usuario = ?, contrasena = ?, estado = ?, email = ?, nombres = ?, apellidos = ? WHERE usuario_id = '$user_id'");
+                $stmt->bind_param('ssssss', $user, $password, $status, $email, $name, $last_name);
+            }
+        } else {
+            $stmt = $conn->prepare("UPDATE tbl_usuario SET usuario = ?, estado = ?, email = ?, nombres = ?, apellidos = ? WHERE usuario_id = '$user_id'");
+            $stmt->bind_param('sssss', $user, $status, $email, $name, $last_name);
+        }
+
+        $stmt->execute();
+        if (($stmt) && ($stmt->affected_rows == 1)) {
+            echo '<script type="text/javascript">
+            alert("Se actualizó la información de: ' . $user . '");
+            window.location.href = "../frm_registrar-usuarios.php";
+            </script>';
+        } else {
+            echo '<script type="text/javascript">
+            alert("No se actualizó la información");
+            window.location.href = "../frm_registrar-usuarios.php";
+            </script>';
+        }
     } else {
-        $stmt = $conn->prepare("UPDATE tbl_usuario SET usuario = ?, estado = ?, email = ?, nombres = ?, apellidos = ? WHERE usuario_id = '$user_id'");
-        $stmt->bind_param('sssss', $user, $status, $email, $name, $last_name);
-    }
-
-    $stmt->execute();
-    if (($stmt) && ($stmt->affected_rows == 1)) {
         echo '<script type="text/javascript">
-        alert("Se actualizó la información de: ' . $user . '");
+        alert("El nombre de usuario ya está registrado");
         window.location.href = "../frm_registrar-usuarios.php";
         </script>';
-    } else {
-        echo '<script type="text/javascript">
-        alert("No se actualizó la información");
-        window.location.href = "../frm_registrar-usuarios.php";
-        </script>';
     }
-    $stmt->close();
-    $conn->close();
 }
